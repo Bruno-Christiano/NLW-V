f2b37ad3c229476c92043220d21daa60369d5c4d
Bruno <bruno_cris_santos@hotmail.com>
2021-04-22 00:24:43 -0300

    configuraçoes

--- src/repositories/SettingsRepository.ts
+++ src/repositories/SettingsRepository.ts
@@ -1,6 +1,7 @@
 
 import { EntityRepository, Repository } from "typeorm";
-import { Setting } from "../models/Setting";
+import { Setting } from "../entities/Setting";
+
 
  
  @EntityRepository(Setting)
--- src/routes.ts
+++ src/routes.ts
@@ -1,35 +1,14 @@
 import { Router } from 'express'
-import { getCustomRepository } from 'typeorm';
+import { SettingsController } from './controllers/SettingsController';
 
-import { SettingsRepository } from './repositories/SettingsRepository';
 
 const routes = Router();
 
-routes.post("/settings", async (req, res) => {
-    const { chat, username } = req.body
+const settingsController = new SettingsController()
 
-    const settingsRepository = getCustomRepository(SettingsRepository)
+routes.post("/settings", settingsController.create)
 
-    const settings = settingsRepository.create({
-        chat,
-        username
-    });
-
-    await settingsRepository.save(settings)
-
-    return res.status(200).json(settings);
-
-
-    // const newSettings = req.body
-    // try {   
-    //  const createsettings = settingsRepository.create(newSettings) 
-    //  return res.status(200).json(createsettings)
-    // } catch (error) {
-    //     return res.status(500).json(error.message)
-    // }
-
-
-
-})
-
+routes.get('/home',(response, req)=>{
+    req.json('Vaaaaaaaaaai')
+}) 
 export { routes }
