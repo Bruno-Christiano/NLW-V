b3bc90abc6a9ac14c9b721941b6e25ae508e8dfd
Bruno <bruno_cris_santos@hotmail.com>
2021-04-22 23:58:59 -0300

    implementando a pasta Public

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
 import express from 'express'
+import {createServer} from 'http'
+import path from 'path'
+import {Server, Socket} from 'socket.io'
 
 
 import './database'
 import {routes} from './routes'
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.get('/pages/client',(req, res)=>{
+    return res.render('html/client.html')
+})
+
+const http = createServer(app)
+const io = new Server(http)
+
+io.on("connection", (socket:Socket)=>{
+    console.log('Se conectou', socket.id)
+})
 const port = 3333
 
 app.use(express.json())
